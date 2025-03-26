55f9904b285a098e1f12f4c4362cb1cf667cc694
Tolulope <toluadelekesteve@gmail.com>
2025-03-26 10:05:03 -0700

    chuly simple CRUD react app

--- src/store/baseURL.js
+++ src/store/baseURL.js
@@ -1,2 +1,2 @@
-export const backend_url = "chuly-backend.vercel.app"
-export const currentURL = "chuly-react-app-steve.vercel.app" 
+export const backend_url = "chuly-backend.vercel.app";
+export const currentURL = "chuly-react-app-steve.vercel.app";
